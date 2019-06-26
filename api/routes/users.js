1000b252fcbd63f2b9085a5cf7df4702790ecf63
vreymond <valentinreymond@gmail.com>
2019-06-26 14:49:32 +0200

    Adding controllers stucture of the api

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,108 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
-const User = require('../models/user')
+const UsersController = require('../controllers/users');
+const checkAuth = require('../middleware/check-auth');
 
-router.post('/signup', (req, res, next) => {
-    User.find({ email: req.body.email})
-        .exec()
-        .then(user => {
-            if (user.length >= 1) {
-                return res.status(409).json({
-                    message: 'Mail exists'
-                });
-            } else {
-                bcrypt.hash(req.body.password, 10, (err, hash) => { // salt random string
-                    if (err) {
-                        return res.status(500).json({
-                            error: err
-                        });
-                    } else {
-                        const user = new User({
-                            _id: new mongoose.Types.ObjectId(),
-                            email: req.body.email,
-                            password: hash
-                        }); 
-                        user
-                        .save()
-                        .then(result => {
-                            console.log(result);
-                            res.status(201).json({
-                                message: 'User created'
-                            });
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            res.status(500).json({
-                                error: err
-                            });
-                        });
-                    }
-                });
-            }
-        });
-});
+router.post('/signup', UsersController.users_signup);
 
-router.post('/login', (req, res, next) => {
-    User.find({ email: req.body.email })
-        .exec()
-        .then(user => {
-            if (user.length < 1) {
-                return res.status(401).json({
-                    message: 'Auth failed'
-                })
-            } 
-            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-                if (err) {
-                    return res.status(401).json({
-                        message: 'Auth failed'
-                    });
-                }
-                if (result) {
-                    const token = jwt.sign({
-                        email: user[0].email,
-                        userId: user[0]._id
-                    }, 
-                    process.env.JWT_KEY, 
-                    {
-                        expiresIn: "1h"
-                    },
-                    );
-                    return res.status(200).json({
-                        message: 'Auth succesful',
-                        token: token
-                    })
-                } 
-                res.status(401).json({
-                    message: 'Auth failed'
-                });
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-});
+router.post('/login', UsersController.users_login);
 
-router.delete('/:userId', (req, res, next) => {
-    User.remove({ _id: req.params.userId})
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: 'User deleted'
-            })
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-});
+router.delete('/:userId', checkAuth, UsersController.users_delete);
 
 module.exports = router;
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRhdGFAdGl0aS5jb20iLCJ1c2VySWQiOiI1ZDEzNjhkYTNlZTg1MGMzZjcwZDcwMzAiLCJpYXQiOjE1NjE1NTMxMjIsImV4cCI6MTU2MTU1NjcyMn0.K5tPt3YSAksTkDDTxW4blocyxp0Yq5J_14FWSpBwvIo
