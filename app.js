bccc25decee490069fef506e6d9f6c02188f13dd
vreymond <valentinreymond@gmail.com>
2019-06-26 13:30:06 +0200

    Adding signup and login routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/users');
 
 mongoose.connect('mongodb+srv://vreymond:'+ process.env.MONGO_ATLAS_PW + '@vreymond-cluster-coben.mongodb.net/test?retryWrites=true&w=majority',
 { useNewUrlParser: true });
@@ -35,6 +36,7 @@ app.use((req, res, next) => {
 // Routes wich should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
